f6482d504d7e567b9494d3811f44f3569e8fd14b
EMMANUEL <dammylola299@gmail.com>
2023-01-07 16:29:07 +0100

    Initialized firebase for the recipe directory

---
+++ src/firebase/firebase.js
@@ -0,0 +1,18 @@
+// Firebase imports
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCNeC-ZWlK6mVgWmAZSUMgdCDeiqm2wlzs",
+  authDomain: "recipe-directory-c7619.firebaseapp.com",
+  projectId: "recipe-directory-c7619",
+  storageBucket: "recipe-directory-c7619.appspot.com",
+  messagingSenderId: "1096936814559",
+  appId: "1:1096936814559:web:cb72206cf581ea15c3766a",
+  measurementId: "G-VE9WLSYWB9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
