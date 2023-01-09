b138416bd9871cd9a46fad9fd498db3f9c8d5c4e
EMMANUEL <dammylola299@gmail.com>
2023-01-09 13:15:45 +0100

    fetched document using the onSnapshot method

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,8 +1,18 @@
+// styles import
 import "./Home.css";
+// firebase imports
+import useFetch from "../../hooks/useFetch";
+
 
 const Home = () => {
+    const {documents: recipes, isPending, error} = useFetch('recipes');
+
+    console.log(recipes);
+
     return (
-        <div>Home</div>
+        <div>
+            Home
+        </div>
     );
 }
  
