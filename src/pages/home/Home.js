884d7563ae17ede9e7ff3872117ff8eace22f0ba
EMMANUEL <dammylola299@gmail.com>
2023-01-09 17:04:35 +0100

    modified the home component

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,18 +2,20 @@
 import "./Home.css";
 // firebase imports
 import useFetch from "../../hooks/useFetch";
+// component imports
+import RecipeList from "../../components/RecipeList";
 
 
 const Home = () => {
-    const {documents: recipes, isPending, error} = useFetch('recipes');
-
-    console.log(recipes);
+    const {documents: recipeData, isPending, error} = useFetch('recipes');
 
     return (
-        <div>
-            Home
+        <div className="home">
+            {error && <p className="error">{ error }</p>}
+            {isPending && <p className="loading">Loading...</p>}
+            {recipeData && <RecipeList recipes={recipeData} />}
         </div>
     );
 }
- 
+
 export default Home;
