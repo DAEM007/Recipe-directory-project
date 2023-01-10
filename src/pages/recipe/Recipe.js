d2d91a967c8f47e0f80944fbd9a99c1d4482d04a
EMMANUEL <dammylola299@gmail.com>
2023-01-10 13:45:48 +0100

    fetched a single recipe from the database

--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -1,8 +1,39 @@
-import "./Recipe";
+// all react imports
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+// styles import
+import "./Recipe.css";
+// all firebase imports
+import { db } from "../../firebase/firebase";
+import { collection, doc, onSnapshot } from "firebase/firestore";
+
 
 const Recipe = () => {
+    const { id } = useParams();
+    // const history = useNavigate();
+    const [recipeDoc, setRecipeDoc] = useState(null);
+    const [isPending, setIsPending] = useState(false);
+
+
+    const colRef = collection(db, "recipes");
+    const docRef = doc(colRef, id);
+
+    useEffect(() => {
+        setIsPending(true);
+        onSnapshot(docRef, (doc) => {
+            // console.log(doc.data());
+            setRecipeDoc(doc.data());
+            setIsPending(false);
+        })
+
+    }, [docRef])
+
+
     return (
-        <div>Recipe</div>
+        <div>
+            {!isPending && <div className="loading">Loading...</div>}
+            {recipeDoc && <h3>{ recipeDoc.title }</h3>}
+        </div>
     );
 }
  
