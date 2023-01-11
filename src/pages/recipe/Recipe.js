31bb1a8a548a5fb4171c1e82ee72ba002b6c80b5
EMMANUEL <dammylola299@gmail.com>
2023-01-11 20:45:33 +0100

    Took all the logic to fetch single recipe to the custom fetch hook

--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -1,36 +1,18 @@
 // all react imports
-import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 // styles import
 import "./Recipe.css";
-// all firebase imports
-import { db } from "../../firebase/firebase";
-import { collection, doc, onSnapshot } from "firebase/firestore";
+// hooks import
+import useFetch from "../../hooks/useFetch";
 
 
 const Recipe = () => {
     const { id } = useParams();
-    const [recipeDoc, setRecipeDoc] = useState(null);
-    const [isPending, setIsPending] = useState(false);
-
-
-    const colRef = collection(db, "recipes");
-    const docRef = doc(colRef, id);
-
-    useEffect(() => {
-        setIsPending(true);
-        onSnapshot(docRef, (doc) => {
-            // console.log(doc.data());
-            setRecipeDoc(doc.data());
-            setIsPending(false);
-        })
-
-    }, [docRef])
-
+    const { document: recipeDoc, isPending } = useFetch("recipes", id);
 
     return (
         <div className="recipe">
-            {!isPending && <div className="loading">Loading...</div>}
+            {isPending && <div className="loading">Loading...</div>}
             {recipeDoc && (
                 <>
                     <h2 className="page-title">{recipeDoc.title}</h2>
@@ -46,3 +28,5 @@ const Recipe = () => {
 }
  
 export default Recipe;
+
+
