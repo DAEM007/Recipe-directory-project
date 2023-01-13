0f362bb11672294e02bf851547a171072b9ec898
EMMANUEL <dammylola299@gmail.com>
2023-01-13 16:10:24 +0100

    Worked on the Search bar component to search recipes that match the term

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,38 @@
-// styles
+// all react imports
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+// styles import
 import "./SearchBar.css";
 
-const Searbar = () => {
-    return ();
+
+const Searchbar = () => {
+    const [term, setTerm] = useState('');
+    const navigate = useNavigate();
+
+    const handleSearch = (e) => {
+        e.preventDefault();
+        // console.log(term);
+        // At this point we direct the user to the search page
+        navigate(`/search?q=${term}`);
+        // setTerm('');
+    }
+
+    return (
+        <div className="searchbar">
+            <form onSubmit={handleSearch}>
+                <label htmlFor="search">
+                    <input 
+                        type="text"
+                        id="search"
+                        placeholder="Search"
+                        value={term}
+                        onChange={(e) => setTerm(e.target.value)}
+                        required
+                    />
+                </label>    
+            </form>
+        </div>
+    );
 }
 
-export default Searbar;
+export default Searchbar;
