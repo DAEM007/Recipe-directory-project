96be17edb9042a4dccfce635713dfee7e9313287
EMMANUEL <dammylola299@gmail.com>
2023-01-15 01:03:36 +0100

    Added the not found page for routes that do not match

---
+++ src/components/NotFound.js
@@ -0,0 +1,14 @@
+
+// styles
+import "./NotFound.css";
+
+const NotFound = () => {
+    return (
+        <div className="not-found">
+            <h3>Page Not Found</h3>
+            <p className="error">Sorry!!! can't find resource for this url...</p>
+        </div>
+    );
+}
+ 
+export default NotFound;
