ee3bec0342ec53dc6e3ed60abf629ada4479af2c
EMMANUEL <dammylola299@gmail.com>
2023-01-17 16:29:27 +0100

    Created a Theme context logic template for the recipe directory

---
+++ src/components/context/ThemeContext.js
@@ -0,0 +1,15 @@
+import { createContext } from "react";
+
+export const ThemeContext = createContext();
+
+export function ThemeProvider({ children }){
+
+    // custom logic here...
+
+    return(
+        <ThemeContext.Provider value={ {color: "blue"} }>
+            { children }
+        </ThemeContext.Provider>
+    )
+}
+
