dbcd0827160fad2ff578dfb1054dd154f89f11ed
EMMANUEL <dammylola299@gmail.com>
2023-01-17 16:50:52 +0100

    used the ThemeContext in the NavBar component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,18 @@
 // all react imports
 import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { ThemeContext } from "./context/ThemeContext";
 // styles
 import "./NavBar.css";
 // component imports
 import Searbar from "./SearchBar";
 
 const NavBar = () => {
+    const { color } = useContext(ThemeContext);
+
+
     return (
-       <div className="navbar">
+       <div className="navbar" style={ {background: color} }>
         <nav>
             <Link to="/" className="brand">
                 <h1>Daem cusine</h1>
