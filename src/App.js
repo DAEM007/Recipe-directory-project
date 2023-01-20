64dad9cf2bce09dcec6b45bdc4fb9fe8540a98a6
EMMANUEL <dammylola299@gmail.com>
2023-01-20 11:02:13 +0100

    Applied light/dark mode to the App component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import './App.css';
 // components import
 import NavBar from './components/NavBar';
 import ThemeSelector from './components/ThemeSelector';
+// hooks import
+import useTheme from './hooks/useTheme';
 // pages
 import Home from './pages/home/Home';
 import Create from './pages/create/Create';
@@ -14,19 +16,23 @@ import NotFound from './components/NotFound';
 
 
 function App() {
+  const { mode } = useTheme();
+
   return (
-    <Router>
-      <NavBar />
-      <ThemeSelector />
-        <Routes>
-          <Route path="/" element={ <Home /> }/>
-          <Route path="/create" element={ <Create /> }/>
-          <Route path="/recipe/:id" element={ <Recipe /> }/>
-          <Route path="/search/recipe/:id" element={ <Recipe /> }/>
-          <Route path="/search" element={ <Search /> }/>
-          <Route path='*' element={ <NotFound /> }/>
-        </Routes>
-    </Router>
+    <div className={`App ${mode}`}>
+      <Router>
+        <NavBar />
+        <ThemeSelector />
+          <Routes>
+            <Route path="/" element={ <Home /> }/>
+            <Route path="/create" element={ <Create /> }/>
+            <Route path="/recipe/:id" element={ <Recipe /> }/>
+            <Route path="/search/recipe/:id" element={ <Recipe /> }/>
+            <Route path="/search" element={ <Search /> }/>
+            <Route path='*' element={ <NotFound /> }/>
+          </Routes>
+      </Router>
+    </div>
   );
 }
 
