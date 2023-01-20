b752d67dd86118bbee07070bf350a3085dc94a74
EMMANUEL <dammylola299@gmail.com>
2023-01-20 10:28:08 +0100

    Added the mode-toggle icon for the light and dark mode, used the context to toggle between modes

--- src/components/ThemeSelector.js
+++ src/components/ThemeSelector.js
@@ -1,15 +1,29 @@
 // styles import
 import "./ThemeSelector.css";
+// assets import
+import modeIcon from "../assets/mode-icon.svg";
 // hooks import
 import useTheme from "../hooks/useTheme";
 
 const themeColors = ['#58249c', 'red', 'green']
 
 export default function ThemeSelector() {
-    const { changeColor } = useTheme();
+    const { changeColor, changeMode, mode } = useTheme();
+
+    const toggleMode = () => {
+        changeMode(mode === "dark" ? "light" : "dark")
+        console.log(mode);
+    }
 
   return (
     <div className="theme-selector">
+        <div className="toggle-mode">
+            <img 
+                src={modeIcon}
+                onClick={toggleMode}
+                alt="dark/light-mode"
+            />
+        </div>
         <div className="theme-buttons">
             {themeColors.map(color => (
                 <div 
